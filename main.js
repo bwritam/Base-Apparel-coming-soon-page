9c7a14db2734417431e4ed43b3385444c6f41e33
bwritam <bwritam@gmail.com>
2021-07-13 11:47:26 +0530

    commit commit

--- main.js
+++ main.js
@@ -7,13 +7,10 @@ function validation() {
 	var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 	if (pattern.test(email)) {
 		document.querySelector(".text").innerHTML = "";
-		document.querySelector(".email-box").style.border = "";
-		document.querySelector(".error-image").style.display = "";
 	}
 	else {
 		document.querySelector(".text").innerHTML = "Please provide a valid email";
-		document.querySelector(".email-box").style.border = "2px solid hsla(0, 93%, 68%, 0.9)";
-		document.querySelector(".error-image").style.display = "block";
+		document.querySelector(".email-box").className = "error-image";
 		changeButton();
 		return false;
 	}
